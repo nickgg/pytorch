69592bd0ca94aa7b8f104599624f783c831bcf50
Wanchao Liang <wanchaol.home@gmail.com>
2020-03-20 14:17:56 -0400

    [jit] make jit/rpc share the same PythonFutureWrapper
    This is the initial step towards merging ivalue future and rpc future
    
    ghstack-source-id: ea2827b0b85e2609fd8aa10dcb250b22b6da978d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/35039

--- torch/csrc/jit/python/pybind_utils.h
+++ torch/csrc/jit/python/pybind_utils.h
@@ -46,6 +46,14 @@
 namespace torch {
 namespace jit {
 
+// The PythonFutureWrapper for ivalue::Future
+struct PythonFutureWrapper {
+  explicit PythonFutureWrapper(c10::intrusive_ptr<c10::ivalue::Future> fut)
+      : fut(std::move(fut)) {}
+
+  c10::intrusive_ptr<c10::ivalue::Future> fut;
+};
+
 // error reporting: when reporting user-caused errors, these functions should
 // not use AT_ERROR macros, since these macros add stack trace information
 // that is confusing to display to the end user since it always reports
@@ -588,13 +596,15 @@ inline IValue toIValue(
     case TypeKind::CapsuleType: {
       return IValue::make_capsule(
           py::cast<c10::intrusive_ptr<CustomClassHolder>>(obj));
-    } break;
+    }
+    case TypeKind::FutureType: {
+      return obj.cast<PythonFutureWrapper>().fut;
+    }
     case TypeKind::AnyType:
       return toTypeInferredIValue(obj);
     case TypeKind::FunctionType:
     case TypeKind::GeneratorType:
     case TypeKind::VarType:
-    case TypeKind::FutureType:
     case TypeKind::QSchemeType:
     case TypeKind::AnyListType:
     case TypeKind::AnyTupleType:
@@ -773,6 +783,8 @@ inline py::object toPyObject(IValue ivalue) {
     return py::reinterpret_borrow<py::object>(ivalue.toPyObject());
   } else if (ivalue.isCapsule()) {
     return py::cast(ivalue.toCapsule());
+  } else if (ivalue.isFuture()) {
+    return py::cast(PythonFutureWrapper(ivalue.toFuture()));
   } else if (ivalue.isRRef()) {
 #ifdef USE_DISTRIBUTED
     return py::cast(torch::distributed::rpc::PyRRef(
